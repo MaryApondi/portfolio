289a309595e74a823ce86796c30969c15edc1cfa
Mary Apondi <apondimary457@gmail.com>
2019-09-03 17:15:08 +0300

    removed portfolio

--- src/Routes.js
+++ src/Routes.js
@@ -5,7 +5,7 @@ import { Route } from 'react-router-dom';
 // COMPONENTS
 import About from './components/About';
 import Contact from './components/Contact'
-import Home from './components/Home'
+// import Home from './components/Home'
 import Landing from './components/Landing'
 
 
@@ -13,7 +13,7 @@ export default function BaseRoutes() {
     return (
         <div>
             <Route exact path = '/' component = {Landing}/>
-            <Route exact path = "/portfolio" component = {Home}/>
+            // <Route exact path = "/portfolio" component = {Home}/>
             <Route exact path = '/about' component = {About} />
             <Route exact path = "/contact" component = {Contact} />
         </div>
