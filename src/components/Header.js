a04693f0b50aa73bc709e65626134abe258c494d
Mary Apondi <apondimary457@gmail.com>
2019-09-06 13:40:56 +0300

    changes made

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Header() {
     return (
             <header>
+            
                 <a href='/' className='logo'>Mary Apondi</a> 
                 <nav className="Navbar">
                     <ul>
